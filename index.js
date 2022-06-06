1225c36893e890631ecbf1c416750be21c921b83
Abraham Perez - a0p0cdd <Abraham.Perez0@walmart.com>
2022-06-06 13:04:14 -0500

    adds splash and hero page

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 $(window).load(function() {
   var msDuration = $('html').css('--splash-screen-duration').slice(0,5);
-
+  
   $(".splash-screen").delay(msDuration).queue(function() {
     $(this).remove();
-  })
-
+    // $('.hide').removeClass();
+  });
+  
   $("#navbar").css("top", "0");
 })
