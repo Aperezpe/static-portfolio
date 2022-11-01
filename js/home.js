ef6421516bcd9aa2b3d61aed95c4afda1049b862
Abraham Perez - a0p0cdd <Abraham.Perez0@walmart.com>
2022-11-01 17:12:22 -0500

    makes scroll animations and all links work

---
+++ js/home.js
@@ -0,0 +1,34 @@
+// Method that identifies scroll percentage of an element
+
+const elementInView = (el, percentageScroll = 100) => {
+const elementTop = el.getBoundingClientRect().top;
+return (
+  elementTop <= 
+  ((window.innerHeight || document.documentElement.clientHeight) * (percentageScroll/100))
+  );
+};
+
+
+const displayElement = (element) => {
+  element.classList.add("scrolled");
+}
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach(el => {
+    if(elementInView(el, 85)) {
+      displayElement(el);
+    }
+  });
+}
+
+let scrollElements;
+  
+window.onload = () => {
+  scrollElements = document.querySelectorAll('.js-scroll');
+  console.log(scrollElements);
+}
+
+window.onscroll = () => {
+  handleScrollAnimation();
+}
+
