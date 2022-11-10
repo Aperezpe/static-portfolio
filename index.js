cdfd83a6c6400f7cb5fb90a6a8036bce77ffe24f
Abraham Perez - a0p0cdd <Abraham.Perez0@walmart.com>
2022-11-10 17:37:58 -0600

    splashscreen fix5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
   var msDuration = $('html').css('--splash-screen-duration').slice(0, 5);
   
-  $(".splash-screen").delay(msDuration).queue(function () {
-    $(this).remove();
-  });
+  setTimeout(() => {
+    $(".splash-screen").remove();
+  }, msDuration);
 });
