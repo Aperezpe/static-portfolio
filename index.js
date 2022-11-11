f2a09eb298d9d061903b9a71e7800bc7f1f538de
Abraham Perez - a0p0cdd <Abraham.Perez0@walmart.com>
2022-11-10 18:11:18 -0600

    splashcreen remove overflow-y

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ $(document).ready(() => {
   //TODO: Do this dynamically
   // var msDuration = $('html').css('--splash-screen-duration').slice(0, 5);
 
+  $(document.body).css('overflow-y', 'hidden');
+
   setTimeout(() => {
     $(".splash-screen").remove();
+    $(document.body).css('overflow-y', 'auto');
+
   }, 2000);
 });
