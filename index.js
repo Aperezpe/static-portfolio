95f91f46ad89420ca65ae7c52708d3bd21d538e7
Abraham Perez - a0p0cdd <Abraham.Perez0@walmart.com>
2022-11-14 10:08:55 -0600

    Revert "Merge branch 'main' of https://github.com/Aperezpe/static-portfolio"
    This reverts commit 04f94e5bae9e3cdbdd7c729a0f2d4366e178d6a3, reversing
    changes made to c0c127d88112a4d2877a54619128d7c46dcdcc72.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/**
- * onLoad triggers before all elements have been loaded on DOM
- */
 $(document).ready(() => {
   //TODO: Do this dynamically
   var msDuration = $('html').css('--splash-screen-duration');
@@ -12,18 +9,4 @@ $(document).ready(() => {
     $(document.body).css('overflow-y', 'auto');
 
   }, msDuration);
-
 });
-
-const getPorfolioData = async () => {
-  let portfolioData = await (await fetch('portfolio-data.json')).json();
-
-  if (!sessionStorage.getItem('portfolioData')) 
-    sessionStorage.setItem('portfolioData', JSON.stringify(portfolioData));
-
-  return portfolioData;
-}
-
-const onLoad = () => {
-  storeScrollElements();
-}
