a9bc1428cb8e56993e34a2c4c18dd4054ac58d78
Abraham Perez - a0p0cdd <Abraham.Perez0@walmart.com>
2022-11-14 20:53:56 -0600

    tries to mimic php app

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("resume");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("index.js");
+  eleventyConfig.addPassthroughCopy("app.json");
+  eleventyConfig.addPassthroughCopy("index.php");
 
   eleventyConfig.addNunjucksFilter('niceDate', (dateObj) => {
     return DateTime.fromJSDate(new Date(dateObj)).toFormat("LLL yyyy");
